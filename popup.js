43bdd77cf37c514dc84041588020bacb732e4c94
Jeremy <jeremy@Jeremys-MacBook-Air.local>
2022-02-25 16:17:56 -0500

    nothing important

--- popup.js
+++ popup.js
@@ -2,6 +2,8 @@ let textarea = document.querySelector(".main");
 let saved = document.querySelector(".saved");
 
 showSavedText();
+inactive = 2;
+setInterval(displaySavedMessage, 300);
 
 window.addEventListener("keyup", saveText);
 window.addEventListener("keydown", keyBeingPressed);
@@ -9,11 +11,24 @@ window.addEventListener("keydown", keyBeingPressed);
 
 function saveText(){
     let userText = textarea.value;
-    saved.textContent = "Saved note"
     localStorage.setItem("data", userText);
+    localStorage.setItem()
+    inactive = 0;
+}
+
+function displaySavedMessage(){
+    if (inactive == 3){
+        saved.textContent = "Saved note"
+        inactive = 0;
+    }
+    else{
+        inactive += 1;
+    }
+
 }
 
 function keyBeingPressed(){
+    inactive = 0;
     saved.textContent = "..."
 }
 
